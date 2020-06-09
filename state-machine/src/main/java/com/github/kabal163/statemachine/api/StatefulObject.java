4a7a3a5ada0e9374d6ede7cb3ea11f95b71d5e14
Dmitrii <cs159753@yandex.ru>
2020-06-09 22:54:51 +0300

    added java docs for TransitionResult and StatefulObject

--- state-machine/src/main/java/com/github/kabal163/statemachine/api/StatefulObject.java
+++ state-machine/src/main/java/com/github/kabal163/statemachine/api/StatefulObject.java
@@ -1,10 +1,34 @@
 package com.github.kabal163.statemachine.api;
 
+/**
+ * Any entity which is needed to be managed by {@link LifecycleManager}
+ * lifecycle manager must implement this interface. This provides
+ * the minimum of methods which are necessary for executing a transition.
+ *
+ * @param <S> a state of the stateful object
+ */
 public interface StatefulObject<S> {
 
+    /**
+     * Returns the stateful object's id
+     *
+     * @param <T> type of id
+     * @return a stateful object's id
+     */
     <T> T getId();
 
+    /**
+     * Returns the actual state of the stateful object
+     *
+     * @return the actual state of the stateful object
+     */
     S getState();
 
+    /**
+     * Sets the state to the stateful object. Should not be
+     * used outside of the {@link LifecycleManager} lifecycle manager
+     *
+     * @param state new state to the stateful object
+     */
     void setState(S state);
 }
--- state-machine/src/main/java/com/github/kabal163/statemachine/api/TransitionResult.java
+++ state-machine/src/main/java/com/github/kabal163/statemachine/api/TransitionResult.java
@@ -3,13 +3,45 @@ package com.github.kabal163.statemachine.api;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Contains the main information about executed transition.
+ * If there were no exceptions then {@code exception} field
+ * will be {@code null}.
+ *
+ * @param <S> a state of a stateful object
+ * @param <E> an event which triggers a transition
+ */
 @Getter
 @RequiredArgsConstructor
 public class TransitionResult<S, E> {
 
+    /**
+     * Whether the transition was successful or not
+     */
     private final boolean success;
+
+    /**
+     * The shared context of the transition
+     */
     private final StateContext<S, E> stateContext;
+
+    /**
+     * The state which stateful object has at the moment
+     * of start the transition
+     */
     private final S sourceState;
+
+    /**
+     * The state which stateful object should has at the moment
+     * of finish the transition. This is the desired state which may
+     * doesn't match the actual state of the stateful object after
+     * transition execution due to exceptions.
+     */
     private final S targetState;
+
+    /**
+     * Any exception which happened during actions or conditions execution
+     * {@code null} if nothing happened
+     */
     private final Exception exception;
 }
